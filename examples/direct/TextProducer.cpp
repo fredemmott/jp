ad76b4b63708294397cae27a5ecaa92ee08b0935
Fred Emmott <mail@fredemmott.co.uk>
2010-09-05 12:10:27 +0100

    add C++ producer

---
+++ examples/direct/TextProducer.cpp
@@ -0,0 +1,29 @@
+#include "Jp.h"
+
+#include <protocol/TBinaryProtocol.h>
+#include <transport/TSocket.h>
+#include <transport/TTransportUtils.h>
+
+#include <iostream>
+
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using boost::shared_ptr;
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+	shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+	shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+	JpClient client(protocol);
+	transport->open();
+
+	cout << "Adding a chicken..." << endl;
+	client.add("text", "chicken");
+	cout << "Added a chicken." << endl;
+	return 0;
+}
