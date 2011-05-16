776c49a7267c6c648a8c7f34cb8468fa69a43993
Fred Emmott <mail@fredemmott.co.uk>
2011-05-16 12:26:05 +0100

    add support for yaml configuration files

--- lib/rb/jp/server/configuration_loader.rb
+++ lib/rb/jp/server/configuration_loader.rb
@@ -1,10 +1,41 @@
 #!/usr/bin/env ruby
+
+autoload :YAML, 'yaml'
+
 # Find a config file and load it
 module Jp
   module Server
     module ConfigurationLoader
     	def self.server_config
-    		load config_file('jp-config')
+    		name = config_file('jp-config')
+        if name =~ /.ya?ml$/
+          load_yaml_config_file(name)
+        else
+          load_ruby_config_file(name)
+        end
+    	end
+
+    	private
+      def self.load_yaml_config_file file
+        config = YAML.load(File.read(file))
+        symbolize_hash(config)
+      end
+
+      def self.symbolize_hash hash
+        hash.dup.each do |k,v|
+          if v.is_a? Hash
+            symbolize_hash v
+          end
+          if k.is_a? String
+            hash.delete k
+            hash[k.to_sym] = v
+          end
+        end
+        hash
+      end
+
+      def self.load_ruby_config_file file
+        load file
     		c = JpConfig.new
     		def c.options
     			m = self.public_methods - Object.public_methods - [:options]
@@ -15,8 +46,8 @@ module Jp
     			h
     		end
     		c.options
-    	end
-    	private
+      end
+
     	def self.config_file name
     		if ARGV[0] && File.exists?(ARGV[0])
     			return File.expand_path ARGV[0]
